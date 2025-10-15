fda3b2e39893ad9e9c6e670433951f44e0f956c7
BaoVy2810 <vytnb234112e@st.uel.edu.vn>
2025-10-15 12:09:19 +0700

    Submit exercise 54

---
+++ Exercise54/math.js
@@ -0,0 +1,20 @@
+function find_max(a,b,c)
+{
+    return Math.max(a,b,c)
+}
+function find_min(a,b,c)
+{
+    return Math.min(a,b,c)
+}
+function find_sin(a)
+{
+    return Math.sin(a * Math.PI/180)
+}
+function find_cos(a)
+{
+    return Math.cos(a * Math.PI/180)
+}
+function find_pow(a,b)
+{
+    return Math.pow(a,b)
+}
